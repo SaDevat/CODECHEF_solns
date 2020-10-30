283fb9976350a64b59ea741eac703cc8c1f31428
Saakshya Devat <s.devat@gmail.com>
2020-10-31 00:32:30 +0530

    Add files via upload

---
+++ AUG LONG.cpp
@@ -0,0 +1,118 @@
+// CHEF WARS - RETURN OF THE JEDI // solved
+
+// #include<bits/stdc++.h>
+// int main()
+// {
+//     std::ios_base::sync_with_stdio(false);
+//     std::cin.tie(NULL);
+
+//     int n; std::cin>>n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int h, p;
+//         std::cin>>h>>p;
+//         while(h>0 && p>0)
+//         {
+//             h = h - p;
+//             p = p/2;
+//         }
+//         if (h>0)
+//         {
+//             std::cout<<0<<"\n";
+//         }
+//         else
+//         {
+//             std::cout<<1<<"\n";
+//         }
+//     }
+// }
+
+// CHEF AND LINEAR CHESS //unsolved
+// BELOW CODE IS GETTING SIGSTP while submission
+
+// #include <bits/stdc++.h>
+// int main()
+// {
+    // std::ios_base::sync_with_stdio(false);
+    // std::cin.tie(NULL);
+//     int t;
+//     std::cin>>t;
+//     for (size_t i = 0; i < t; i++)
+//     {
+//         int n, k;
+//         std::cin>>n>>k;
+//         std::vector<int> v;
+//         std::make_heap(v.begin(), v.end());
+//         for (size_t i = 0; i < n; i++)
+//         {
+//             int temp;
+//             std::cin>>temp;
+//             if(k%temp==0)
+//             {v.push_back(temp);}
+//         }
+//         if(!v.empty())
+//             std::cout<<v.front()<<"\n";
+//         else
+//         {
+//             std::cout<<-1<<"\n";
+//         }
+//     }
+// }
+
+// ANOTHER CARD PROBLEM
+// #include <bits/stdc++.h>
+
+// int main()
+// {
+//     int t;
+//     std::cin>>t;
+//     for (size_t i = 0; i < t; i++)
+//     {
+//         int a, b, x=0, y=0;
+//         std::cin>>a>>b;
+//         if(a%9==0)
+//         {
+//             x = a/9;
+//         }
+//         else
+//         {
+//             x = 1 + a/9;
+//         }
+//         if(b%9==0)
+//         {
+//             y = b/9;
+//         }
+//         else
+//         {
+//             y = 1 + b/9;
+//         }
+//         if(x<y)
+//         {
+//             std::cout<<0<<" "<<x<<"\n";
+//         }
+//         else
+//         {
+//             std::cout<<1<<" "<<y<<"\n";
+//         }
+//     }
+// }
+
+// SMALLEST KMP
+// #include <bits/stdc++.h>
+
+// int main()
+// {
+//     int t;
+//     std::cin>>t;
+//     for (size_t i = 0; i < t; i++)
+//     {
+//         std::string s, p;
+//         std::cin>>s>>p;
+
+//     }
+    
+// }
+
+
+
+// CHEF AND WEDDING ARRANGEMENTS
